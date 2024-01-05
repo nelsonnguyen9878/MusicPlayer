4791b92c7cf0323367b438a183b3cca40e255212
Nelson Nguyen <110303502+nelsonnguyen9878@users.noreply.github.com>
2024-01-04 22:21:43 -0800

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,1 +1,20 @@
 
+// Filename: Playlist.cpp
+// started by Nelson Nguyen 12/29/23
+
+// libraries
+#include <cstdlib>
+#include "Song.h"
+#include "Playlist.h"
+using namespace std;
+
+// constructor definition
+Playlist::Playlist() {
+    head_ptr = NULL; // pointing to no current songs
+    many_songs = 0; // starts off at 0 songs in the playlist
+}
+
+void Playlist::InsertSong(const Playlist::string &add_song, Song *head_ptr) {
+    head_ptr = new Song(add_song, head_ptr);
+    ++many_songs;
+}
