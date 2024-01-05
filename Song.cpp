348ccc228e833de0bf7e08b0a21760625870976c
Nelson Nguyen <110303502+nelsonnguyen9878@users.noreply.github.com>
2024-01-04 22:22:39 -0800

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -1,12 +1,13 @@
 //
 // Started by Abdurrahman Alyajouri on 10/27/2023.
-//
+// edited variables - Nelson
+
 
 #include "Song.h"
 
 Song::Song(const Song::string& data, Song* previous_link, Song* next_link) {
     song_file_name = data;
-    previous_link = previous_song_ptr;
-    next_link = next_song_ptr;
+    previous_song_ptr = previous_link;
+    next_song_ptr = next_link;
     return;
 }
